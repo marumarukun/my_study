e0ea6d9e2941a29d8e9e584f8304ed39faa64de6
marumarukun <cheriera.0310@gmail.com>
2024-03-24 19:45:23 +0900

    キャプチャした画像とその情報を表示するように変更

--- coding-samurai/cp02/03_Samurai_Kaoawase/work/camera_example.py
+++ coding-samurai/cp02/03_Samurai_Kaoawase/work/camera_example.py
@@ -2,14 +2,16 @@ import gradio as gr
 import datetime
 
 def shot(image):
-    print(datetime.datetime.now(), image.size, type(image))
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    image_info = f"撮影時刻: {timestamp}, サイズ: {image.size}, タイプ: {type(image)}"
+    return image, image_info
 
 with gr.Blocks() as demo:
-    # Gradio Ver.3の場合
-    # camera_image = gr.Image(type="pil", source="webcam", streaming=True, width=768, height=512)
     camera_image = gr.Image(type="pil", sources=["webcam"], streaming=True, width=768, height=512)
     button_shot = gr.Button("Shot")
-    button_shot.click(shot, camera_image, None)
+    output_image = gr.Image(type="pil")
+    output_text = gr.Textbox(label="画像情報")
+    button_shot.click(shot, camera_image, [output_image, output_text])
 
 if __name__ == "__main__":
     demo.launch()
